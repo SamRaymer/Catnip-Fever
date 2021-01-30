85ba41a6c9827fa46217611ba28f4d6a0916c1d9
Evan <sinnombre124@gmail.com>
2021-01-30 13:36:52 -0800

    bikes

--- Movement/Assets/Scripts/SpawnManager.cs
+++ Movement/Assets/Scripts/SpawnManager.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
 
+    public GameObject RightCarPrefab;
+    public GameObject LeftCarPrefab;
 
     // Start is called before the first frame update
     void Start()
