d9dec1c0578390700fda76f1d2b8868fad64a911
Jaime <jaime.baker@gmail.com>
2021-01-30 23:10:43 -0500

    We have moving cat

--- Movement/Assets/Scripts/SpawnCats.cs
+++ Movement/Assets/Scripts/SpawnCats.cs
@@ -10,7 +10,7 @@ public class SpawnCats : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(prefabCat, new Vector3(0,0,0), Quaternion.identity);
+        Instantiate(prefabCat, new Vector3(1,14,0), Quaternion.identity);
     }
 
     // Update is called once per frame
