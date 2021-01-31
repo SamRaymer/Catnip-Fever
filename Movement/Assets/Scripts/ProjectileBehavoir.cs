e81427cd187558c7f696b16ca014e1d5c1e5cfc5
Evan <sinnombre124@gmail.com>
2021-01-30 17:30:01 -0800

    Cars and Bikes spawn

--- Movement/Assets/Scripts/ProjectileBehavoir.cs
+++ Movement/Assets/Scripts/ProjectileBehavoir.cs
@@ -10,6 +10,11 @@ public class ProjectileBehavoir : MonoBehaviour
 
     private float direction_radians;
 
+    private float despawnTop = 23.0f;
+    private float despawnBottom = 5.0f;
+    private float despawnLeft = -13.0f;
+    private float despawnRight = 9.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,7 +25,12 @@ public class ProjectileBehavoir : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position += (new Vector3(0,1,0) * Mathf.Sin(direction_radians) + new Vector3(1, 0, 0) * Mathf.Cos(direction_radians)) * velocity * Time.deltaTime;
+        transform.position += (new Vector3(0, 1, 0) * Mathf.Sin(direction_radians) + new Vector3(1, 0, 0) * Mathf.Cos(direction_radians)) * velocity * Time.deltaTime;
         transform.Rotate(0, 0, rotation * Time.deltaTime);
+
+        if (transform.position.x > despawnRight || transform.position.x < despawnLeft || transform.position.y > despawnTop || transform.position.y<despawnBottom)
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
--- Movement/Assets/Scripts/SpawnManager.cs
+++ Movement/Assets/Scripts/SpawnManager.cs
@@ -5,18 +5,73 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
 
-    public GameObject RightCarPrefab;
-    public GameObject LeftCarPrefab;
+    public GameObject UpCarPrefab;
+    public GameObject DownCarPrefab;
+    public GameObject UpBikePrefab;
+    public GameObject DownBikePrefab;
+
+    public float NextLDBike;
+    public float NextLUBike;
+    public float NextDCar;
+    public float NextUCar;
+    public float NextRDBike;
+    public float NextRUBike;
+
+    public float BikeInterval = 2.0f;
+    public float CarInterval = 2.0f;
+
+    private Vector3 LDBikeSpawn = new Vector3(-7.9f,20f,0f);
+    private Vector3 LUBikeSpawn = new Vector3(-7.63f,8f,0f);
+    private Vector3 DCarSpawn = new Vector3(-6.28f,20f,0f);
+    private Vector3 UCarSpawn = new Vector3(-5.03f,8f,0f);
+    private Vector3 RDBikeSpawn = new Vector3(-3.69f,20f,0f);
+    private Vector3 RUBikeSpawn = new Vector3(-3.34f,8f,0f);
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        NextLDBike = Time.time + BikeInterval * (1 + Random.Range(0, 1));
+        NextLUBike = Time.time + BikeInterval * (1 + Random.Range(0, 1));
+        NextDCar =   Time.time + CarInterval * (1 + Random.Range(0, 1));
+        NextUCar =   Time.time + CarInterval * (1 + Random.Range(0, 1));
+        NextRDBike = Time.time + BikeInterval * (1 + Random.Range(0, 1));
+        NextRUBike = Time.time + BikeInterval * (1 + Random.Range(0, 1));
+}
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Spawn bikes and cars
+        if(Time.time > NextLDBike)
+        {
+            NextLDBike = Time.time + BikeInterval * (1 + Random.Range(0, 1));
+            Instantiate(DownBikePrefab, LDBikeSpawn, new Quaternion(0f, 0f, 0f, 0f));
+        }
+        if (Time.time > NextLUBike)
+        {
+            NextLUBike = Time.time + BikeInterval * (1 + Random.Range(0, 1));
+            Instantiate(UpBikePrefab, LUBikeSpawn, new Quaternion(0f, 0f, 0f, 0f));
+        }
+        if (Time.time > NextDCar)
+        {
+            NextDCar = Time.time + CarInterval * (1 + Random.Range(0, 1));
+            Instantiate(DownCarPrefab, DCarSpawn, new Quaternion(0f, 0f, 0f, 0f));
+        }
+        if (Time.time > NextUCar)
+        {
+            NextUCar = Time.time + CarInterval * (1 + Random.Range(0, 1));
+            Instantiate(UpCarPrefab, UCarSpawn, new Quaternion(0f, 0f, 0f, 0f));
+        }
+        if (Time.time > NextRDBike)
+        {
+            NextRDBike = Time.time + BikeInterval * (1 + Random.Range(0, 1));
+            Instantiate(DownBikePrefab, RDBikeSpawn, new Quaternion(0f, 0f, 0f, 0f));
+        }
+        if (Time.time > NextRUBike)
+        {
+            NextRUBike = Time.time + BikeInterval * (1 + Random.Range(0, 1));
+            Instantiate(UpBikePrefab, RUBikeSpawn, new Quaternion(0f, 0f, 0f, 0f));
+        }
     }
 }
